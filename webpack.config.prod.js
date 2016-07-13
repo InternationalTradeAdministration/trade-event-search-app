7ea8cf1199847cc59a142870ab8524e45b775ec7
Victor Lim <victor@rrsoft.co>
2016-07-13 17:06:49 +0800

    fix title

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,7 +4,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: false,
   entry: [
     './src/index',
   ],
