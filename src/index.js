6ec22b12bb345882db835f2a52e25b2844baf814
Victor Lim <victor@rrsoft.co>
2016-09-01 16:41:42 +0800

    use react-hot-loader@3.0 API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { AppContainer } from 'react-hot-loader';
 import createHistory from 'history/lib/createHashHistory';
 import useQueries from 'history/lib/useQueries';
 import configureStore from './configureStore';
@@ -12,19 +11,22 @@ function renderToElement(elementId) {
   const history = useQueries(createHistory)();
 
   render(
-    <AppContainer>
     <Provider store={store} key="provider">
       <App history={history} />
-    </Provider></AppContainer>, document.getElementById(elementId));
+    </Provider>, document.getElementById(elementId));
 
-  if (module.hot) {
+  if (__DEVELOPMENT__ && module.hot) {
+    const { AppContainer } = require('react-hot-loader');
+
     module.hot.accept('./containers/App', () => {
-      const Next = require('./containers/App').default;
+      const NextApp = require('./containers/App').default;
+
       render(
         <AppContainer>
           <Provider store={store} key="provider">
-            <App history={history} />
-        </Provider></AppContainer>, document.getElementById(elementId));
+            <NextApp history={history} />
+          </Provider>
+        </AppContainer>, document.getElementById(elementId));
     });
   }
 }
