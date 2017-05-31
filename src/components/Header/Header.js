987f64f452d628118b414115c1f8c5b310d7c10b
Victor Lim <victor@rrsoft.co>
2017-05-31 16:59:10 +0800

    Add subheader

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Header.scss';
+
+export default () => (
+  <div>
+    <div className="explorer__header">Trade Events Search</div>
+    <div className="explorer__subheader">Connect with foreign buyers and expand international sales by attending one of our global trade events!</div>
+  </div>
+);
